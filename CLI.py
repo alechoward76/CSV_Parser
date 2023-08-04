13b46c4442444b0091c453d46a86b7704df1ede1
Alec Howard <95328178+alechoward76@users.noreply.github.com>
2023-08-04 11:51:56 -0500

    Update CLI.py

--- CLI.py
+++ CLI.py
@@ -1,43 +1,54 @@
-# Creating a Command Line Interface for smoother user experience
-
 # CSV_CLI
+# Alec Howard
+# 08/4/2023
+
+#!/usr/bin/env python3
+
+
 import argparse
-from pathlib import Path
-import datetime
-
-# create argument parser
-parser = argparse.ArgumentParser()
-parser.add_argument("path")
+import pandas
 
 
-# Add args
-parser.add_argument("-l", "--long", action="store_true")
+def main():
+    # Create Parser
+    parser = argparse.ArgumentParser(description="A tool for parsing csv files")
+
+    # Define command line arguments
+    parser.add_argument("positional_arg", type=str, help="name of csv file")
+
+    # What is the difference between these two?v
+    """
+    parser.add_argument(
+        "-o", "--optional arg", type=int, default=0, help="Description of optional arg"
+    )
+    """
+    parser.add_argument(
+        "--flag", action="store_true", help="Description of a flag option"
+    )
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+    try:
+        # Access and use the parsed args
+        print("Positional Argument: ", args.positional_arg)
+
+        print("Flag Option: ", args.flag)
+
+        # Add logic based on parsed args
+        if args.flag:
+            print("Flag is set!")
+        else:
+            print("Flag is not set.")
+        """
+        if hasattr(args, "optional arg"):
+            result = args.optional_arg * 2
+            print("Optional Argument: ", args.optional_arg)
+            print("Result ", result)
+        """
+    # Error handle
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
-args = parser.parse_args()
-
-# Setup target directory of script
-target_dir = Path(args.path)
-
-# alert if directory is nonexistent
-if not target_dir.exists():
-    print("The target does not exist")
-    raise SystemExit(1)
-
-
-# build output
-def build_output(entry, long=False):
-    if long:
-        size = entry.stat().st_size
-
-        date = datetime.datetime.fromtimestamp(entry.stat().st_mtime).strftime(
-            "%b %d %H:%M:%S"
-        )
-        return f"{size:>6d} {date} {entry.name}"
-    return entry.name
-
-
-# Print name of dir
-for entry in target_dir.iterdir():
-    print(build_output(entry, long=args.long))
-
+if __name__ == "__main__":
+    main()
