ab35d07cd7022476cdcc75c8f7b39f876e2a8c98
Alec Howard <95328178+alechoward76@users.noreply.github.com>
2023-08-04 12:42:38 -0500

    Update CSV_Parser.py
    whoops

--- CSV_Parser.py
+++ CSV_Parser.py
@@ -1,69 +1,152 @@
-# CSV_CLI
 # Alec Howard
-# 08/4/2023
+# 06/15/2023
+# CSV Parser
 
-#!/usr/bin/env python3
-
-
-import argparse
+# Imports
+import os
 import pandas as pd
 
-
-def main():
-    # Create Parser
-    parser = argparse.ArgumentParser(description="A tool for parsing csv files")
-
-    # Define command line arguments
-    parser.add_argument("positional_arg", type=str, help="name of csv file")
-
-    # What is the difference between these two?v
-
-    parser.add_argument(
-        "-a", "--optional arg", type=str, help="Description of optional arg"
-    )
-
-    parser.add_argument(
-        "--flag", action="store_true", help="Description of a flag option"
-    )
-
-    parser.add_argument(
-        "values", nargs="*", help="Input columns to add to the output document"
-    )
-
-    # Parse the command line arguments
-    args = parser.parse_args()
+while 1 == 1:
+    # Input Username and File Name
     try:
-        # Read file
-        df = pd.read_csv(args.positional_arg)
+        print("Enter Username For File Path: ")
+        userName = input()
+        print("\nEnter Name of CSV File From Documents Folder (including extension): ")
+        fileName = input()
 
-        # Add Column to Excel Sheet
+        # Create Dataframe from input csv file
+        df = pd.read_csv(r"C:\Users\\" + userName + "\\Documents" + "\\" + fileName)
+        break
+    except:
+        print("INVALID PATH: Retry \n")
 
-        # Access and use the parsed args
-        # print("Positional Argument: ", args.positional_arg)
 
-        # print("Flag Option: ", args.flag)
+# column List I want parsed out
+selected_columns = []
 
-        # Add logic based on parsed args
-        if args.flag:
-            print("Flag is set!")
+# Start Main While Loop
+while 1 == 1:
+    print("\nEnter a Number to Select an Option: \n")
+    print("1. Add Column")
+    print("2. Count Occurrences of an Item in Specific Column")
+    print(
+        "3. Remove Duplicate Rows or Rows containing NaNs from Dataframe of Selected Columns"
+    )
+    print("4. Exit & Export")
+    userInput = input()
+
+    # Add Column to Excel Sheet
+    if userInput == "1":
+        try:
+            # show all columns
+            print("\nColumns: \n")
+            print(df.columns)
+
+            # Enter Column to Select
+            print("\n")
+            print("Enter Column Name to Add \n")
+            columnName = input()
+
+            # Throw if the columnName is invalid
+            testValid = df[columnName]
+
+            # Check if Column is already selected
+            if columnName not in selected_columns:
+                selected_columns.append(columnName)
+            else:
+                print("\nYou Already Selected this Column!")
+
+            print("\nSelected Column(s) " + str(selected_columns) + "\n")
+        except:
+            print("INVALID COLUMN NAME\n")
+
+    # Count occurences of a specific item in a specific column
+    elif userInput == "2":
+        try:
+            # Select Column to Search
+            print("\nColumns: \n")
+            print(df.columns)
+            print("\nEnter Column Name to Search: \n")
+            columnName = input()
+
+            # Drop Duplicates and NAN when displaying item options in column
+            print("\n")
+            print(df[columnName].drop_duplicates().dropna())
+            print("\n")
+
+            # Selecting Item to search for in column
+            print("\nEnter Item to Search: \n")
+            item_to_search = input()
+            item_counts = df[columnName].value_counts()[item_to_search]
+            print(item_to_search + ": " + str(item_counts) + " Occurrences")
+            print("\n")
+            print("Quit Without Exporting? (y/n) \n")
+            userChoice = input()
+            if userChoice == "y":
+                print("\nGoodbye! (´^ω^)ノ\n")
+                break
+        except:
+            print("INVALID: Retry\n")
+    # Remove Duplicates / NaNs
+    elif userInput == "3":
+        print("\n")
+        print("Untrimmed Dataframe: ")
+        print(df[selected_columns])
+        print("\n")
+        # Select Choice
+        print("\nSelect a Number to Continue: \n")
+        print("1. Remove Duplicate Rows")
+        print(
+            "2. Remove Rows Containing NaNs (WARNING: Any Row Containing a NaN Will be Erased from Dataframe)"
+        )
+        print("3. Exit")
+        option = input()
+
+        # Remove Duplicates
+        if option == "1":
+            df = df.drop_duplicates()
+        # Remove NaN
+        elif option == "2":
+            df = df.dropna()
+        # Exit
         else:
-            print("Flag is not set.")
+            continue
+        print("\n")
+        print(df[selected_columns])
+        print("\n")
 
-        # Save columns to doc
-        if args.values:
-            print("Input Values:", args.values)
-            columns = df[args.values]
-            columns.to_excel("output.xlsx")
-        """
-        if hasattr(args, "optional arg"):
-            result = args.optional_arg * 2
-            print("Optional Argument: ", args.optional_arg)
-            print("Result ", result)
-        """
-    # Error handle
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    # Exit & Export
+    elif userInput == "4":
+        try:
+            # pull dataframe data from the selected columns
+            subset_data = df[selected_columns]
+            print(subset_data)
+            print("\nWould You Like to Export to Excel? (y/n)\n")
+            choose = input()
+            if choose == "y":
+                try:
+                    # Export subset data to excel file
+                    print("\nEnter xlsx File Name to Output to (including extension) ")
+                    outputFile = input()
+                    subset_data.to_excel(
+                        r"C:\Users\\" + userName + "\\Documents" + "\\" + outputFile,
+                        index=False,
+                    )
+                    print("\nCheck your Documents Folder ( ͡° ͜ʖ ͡°)\n")
+                    break
+                except:
+                    print("\nInvalid File Name")
+            elif choose == "n":
+                print("\nGoodbye! (´^ω^)ノ\n")
+                break
+            else:
+                print("\nInvalid Option\n")
 
+        except:
+            # Reset Selection Process if an Invalid Column is Provided
+            print("How did you get here?")
 
-if __name__ == "__main__":
-    main()
+    # Invalid Option
+    else:
+        print("\nInvalid Option \n")
+# END WHILE LOOP
